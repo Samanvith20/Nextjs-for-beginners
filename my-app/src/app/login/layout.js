fd1707644ccd5c7c988f300f51e8a853e6d59e33
Samanvith <Samanvith2005@gmail.com>
2024-03-13 14:47:11 +0530

    basic Installation & App Routing

---
+++ my-app/src/app/login/layout.js
@@ -0,0 +1,8 @@
+export default function LoginLayout({ children }) {
+    return (
+        <body>
+            <h1>This is login header</h1>
+            {children}
+        </body>
+    );
+}
---
+++ my-app/src/app/login/products/page.js
@@ -0,0 +1,7 @@
+export default function productsPage(){
+     return (
+        <div>
+            <h1>In this we have to render all the products of my-website</h1>
+        </div>
+     )
+}
